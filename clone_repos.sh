7b2b96e72b3a1d24481b9fd56dbc780d7914cb7e
Nick Rowlands <nick@rowlando.com>
2020-06-10 19:30:40 +0100

    Clone a list of repos into a sub directory

---
+++ clone_repos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+TEMP_GIT_REPOS_DIR=repos
+mkdir -p "$TEMP_GIT_REPOS_DIR"
+cd "$TEMP_GIT_REPOS_DIR"
+
+# clone repos
+while read line; do
+  ../clone_or_pull.sh ${line}
+done < "${1:-/dev/stdin}"
